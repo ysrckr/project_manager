d153540cfb1270a839ef29faf3afd098ad50251a
ysrckr <dyt.yasarcakir@gmail.com>
2022-07-28 15:16:51 +0300

    mobile nav

--- client/components/layouts/Header.tsx
+++ client/components/layouts/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { FC } from 'react'
 import { Link } from 'wouter'
+import { useState } from 'react'
 import styles from '../../src/styles/modules/Header.module.scss'
+import MobileNav from '../layouts/MobileNav'
 const Header: FC = () => {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+	const handleResize = () => {
+		setScreenWidth(window.innerWidth)
+	}
+	useLayoutEffect(() => {
+		window.addEventListener('resize', handleResize)
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [screenWidth])
 	return (
 		<header>
 			<div className={styles.link}>
@@ -15,13 +28,17 @@ const Header: FC = () => {
 					/>
 				</Link>
 			</div>
-			<nav>
-				<ul>
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-				</ul>
-			</nav>
+			{screenWidth > 768 ? (
+				<nav>
+					<ul>
+						<li className={styles.list}>
+							<Link href="/">Home</Link>
+						</li>
+					</ul>
+				</nav>
+			) : (
+				<MobileNav />
+			)}
 		</header>
 	)
 }
