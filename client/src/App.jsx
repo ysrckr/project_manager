73460cc80b07985f3fc6e97cea08c8ad77dbe244
ysrckr <dyt.yasarcakir@gmail.com>
2022-07-29 02:10:51 +0300

    react query and easy state added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route } from 'wouter'
 import Home from './pages/Home'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './queries/queryClient'
 function App() {
 	return (
-		<Route path="/">
-			<Home />
-		</Route>
+		<QueryClientProvider client={queryClient}>
+			<Route path="/">
+				<Home />
+			</Route>
+		</QueryClientProvider>
 	)
 }
 
